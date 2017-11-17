1771ccc20e44de9e38eb85348193c3e0a0a7ec11
tefabarrio <32182909+tefabarrio@users.noreply.github.com>
2017-11-16 22:17:59 -0600

    Update Metodos.java

--- src/Metodos.java
+++ src/Metodos.java
@@ -5,8 +5,10 @@
  */
 import java.util.ArrayList;
 /**
- *
- * @author marianam
+ * @author Alexander Trujillo, Carné 17189
+ * @author Mariana Morales, Carné 17235
+ * @author Estefania Barrio, Carné 17927
+ * @version 11/16/201
  */
 public class Metodos {
     Carrera matematica = new Carrera("matematica");
